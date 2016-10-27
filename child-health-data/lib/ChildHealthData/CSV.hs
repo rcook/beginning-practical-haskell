3d9ccb7ed6b83e7cd8e0fda02fa368f7fa6cb9f6
Richard Cook <rcook@rcook.org>
2016-10-26 19:16:51 -0700

    Add row header

--- child-health-data/lib/ChildHealthData/CSV.hs
+++ child-health-data/lib/ChildHealthData/CSV.hs
@@ -1,11 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 
 module ChildHealthData.CSV
   ( encodeCSV
   ) where
 
+import qualified Data.ByteString.Char8 as SC8
 import qualified Data.ByteString.Lazy.Char8 as C8
 import           Data.Csv
+import qualified Data.Text as T
+import           Data.Text.ICU.Translit
+import qualified Data.Vector as V
 
 import           ChildHealthData.Table
 import           ChildHealthData.Types
@@ -37,5 +42,70 @@ instance ToField SubgroupCategory where
 instance ToField SubgroupValue where
     toField = toField . unSubgroupValue
 
+qHeader :: SC8.ByteString
+qHeader = "question"
+
+aHeader :: SC8.ByteString
+aHeader = "answer"
+
+sgcHeader :: SC8.ByteString
+sgcHeader = "subgroup category"
+
+sgvHeader :: SC8.ByteString
+sgvHeader = "subgroup value"
+
+pHeader :: SC8.ByteString
+pHeader = "%"
+
+lHeader :: SC8.ByteString
+lHeader = "CI - lower"
+
+uHeader :: SC8.ByteString
+uHeader = "CI - upper"
+
+nHeader :: SC8.ByteString
+nHeader = "n"
+
+rowHeaders :: Header
+rowHeaders = V.fromList
+  [ qHeader
+  , aHeader
+  , sgcHeader
+  , sgvHeader
+  , pHeader
+  , lHeader
+  , uHeader
+  , nHeader
+  ]
+
+instance ToNamedRecord Row where
+    toNamedRecord (Row q a sgc sgv p l u n) = namedRecord
+      [ qHeader .= q
+      , aHeader .= a
+      , sgcHeader .= sgc
+      , sgvHeader .= sgv
+      , pHeader .= p
+      , lHeader .= l
+      , uHeader .= u
+      , nHeader .= n
+      ]
+
+asciiTrans :: Transliterator
+asciiTrans = trans "Latin-ASCII"
+
+asciiTransliterate :: T.Text -> T.Text
+asciiTransliterate = transliterate asciiTrans
+
+transliterateRow :: Row -> Row
+transliterateRow (Row q a sgc sgv p l u n) = Row
+  ((Question . asciiTransliterate . unQuestion) q)
+  ((Answer . asciiTransliterate . unAnswer) a)
+  ((SubgroupCategory . asciiTransliterate . unSubgroupCategory) sgc)
+  ((SubgroupValue . asciiTransliterate . unSubgroupValue ) sgv)
+  p
+  l
+  u
+  n
+
 encodeCSV :: Table -> C8.ByteString
-encodeCSV = encode
+encodeCSV rs = encodeByName rowHeaders (map transliterateRow rs)
--- child-health-data/lib/ChildHealthData/Table.hs
+++ child-health-data/lib/ChildHealthData/Table.hs
@@ -12,32 +12,14 @@ import qualified Data.Text as T
 import           Data.Text.ICU.Translit
 import           ChildHealthData.Types
 
-asciiTrans :: Transliterator
-asciiTrans = trans "Latin-ASCII"
-
-asciiTransliterate :: T.Text -> T.Text
-asciiTransliterate = transliterate asciiTrans
-
-type Row = (Question, Answer, SubgroupCategory, SubgroupValue, P, L, U, N)
+data Row = Row Question Answer SubgroupCategory SubgroupValue P L U N
 type Table = [Row]
-
-transliterateRow :: Row -> Row
-transliterateRow (q, a, sgc, sgv, p, l, u, n) =
-  ( (Question . asciiTransliterate . unQuestion) q
-  , (Answer . asciiTransliterate . unAnswer) a
-  , (SubgroupCategory . asciiTransliterate . unSubgroupCategory) sgc
-  , (SubgroupValue . asciiTransliterate . unSubgroupValue ) sgv
-  , p
-  , l
-  , u
-  , n
-  )
 
 answerRows :: SubgroupCategory -> SubgroupValue -> Question -> [(Answer, ValueTuple)] -> Table -> Table
 answerRows sgc sgv question answers rs = foldr (helper sgc sgv question) rs answers
     where
         helper :: SubgroupCategory -> SubgroupValue -> Question -> (Answer, ValueTuple) -> Table -> Table
-        helper sgc sgv question (answer, (p, (l, u), n)) rs = (question, answer, sgc, sgv, p, l, u, n) : rs
+        helper sgc sgv question (answer, (p, (l, u), n)) rs = Row question answer sgc sgv p l u n : rs
 
 subcategoryRows :: GGMap -> Page -> Table -> Table
 subcategoryRows ggMap Page{..} rs = foldr (\(sgv, xyzs) -> answerRows sgc sgv question (zip answers xyzs)) rs subcategories
@@ -45,4 +27,4 @@ subcategoryRows ggMap Page{..} rs = foldr (\(sgv, xyzs) -> answerRows sgc sgv qu
         Just sgc = M.lookup gg ggMap
 
 pageTable :: GGMap -> [Page] -> Table
-pageTable ggMap ps = map transliterateRow $ foldr (subcategoryRows ggMap) [] ps
+pageTable ggMap ps = foldr (subcategoryRows ggMap) [] ps
