eea5138137abd3eb94b156492c1c3c2829c7546f
Richard Cook <rcook@rcook.org>
2016-11-01 16:11:49 -0700

    Cache output from stack exec

--- child-health-data/child-health-data-app.cmd
+++ child-health-data/child-health-data-app.cmd
@@ -1,6 +1,10 @@
 @echo off
 setlocal
+set ICUBINPATHFILE=.stack-work/icu-bin-path.txt
 pushd %~dp0
-for /f %%i in ('stack exec -- sh script/get-icu-bin-path.sh') do set ICUBINPATH=%%i
+if not exist %ICUBINPATHFILE% (
+  stack exec -- sh script/get-icu-bin-path.sh > %ICUBINPATHFILE%
+)
+set /p ICUBINPATH=<%ICUBINPATHFILE%
 PATH=%ICUBINPATH%;%PATH%
 stack exec child-health-data-app.exe %*
