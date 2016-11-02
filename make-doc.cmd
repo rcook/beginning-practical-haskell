d1e5b38ceb773e075006550caa19d4b0c64d8be5
Richard Cook <rcook@rcook.org>
2016-11-02 11:34:56 -0700

    Create LaTeX file and clean up

--- make-doc.cmd
+++ make-doc.cmd
@@ -5,5 +5,11 @@ set PANDOCOPTS=--highlight-style=tango --latex-engine=xelatex --variable mainfon
 set INPUTPATHS=index.md part01.md part02.md part03.md part04.md
 set PANDOCDIR=%APPDATA%\local\bin
 set PANDOCPATH=%PANDOCDIR%\pandoc.exe
+
+del %OUTDIR%\notes.html >nul 2>&1
+del %OUTDIR%\notes.pdf >nul 2>&1
+del %OUTDIR%\notes.tex >nul 2>&1
+
 %PANDOCPATH% %PANDOCOPTS% -s -o %OUTDIR%\notes.html %INPUTPATHS%
 %PANDOCPATH% %PANDOCOPTS% -s -o %OUTDIR%\notes.pdf %INPUTPATHS%
+%PANDOCPATH% %PANDOCOPTS% -s -o %OUTDIR%\notes.tex %INPUTPATHS%
