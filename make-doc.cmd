d004a93a335396dd71288157844f32b6f5a83ba7
Richard Cook <rcook@rcook.org>
2016-11-02 11:07:40 -0700

    Generate PDF

--- make-doc.cmd
+++ make-doc.cmd
@@ -1,8 +1,9 @@
 @echo off
 setlocal
 set OUTDIR=doc
-set PANDOCOPTS=--highlight-style=tango
+set PANDOCOPTS=--highlight-style=tango --latex-engine=xelatex --variable mainfont="Times New Roman"
 set INPUTPATHS=index.md part01.md part02.md part03.md part04.md
 set PANDOCDIR=%APPDATA%\local\bin
 set PANDOCPATH=%PANDOCDIR%\pandoc.exe
 %PANDOCPATH% %PANDOCOPTS% -s -o %OUTDIR%\notes.html %INPUTPATHS%
+%PANDOCPATH% %PANDOCOPTS% -s -o %OUTDIR%\notes.pdf %INPUTPATHS%
