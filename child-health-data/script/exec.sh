8e1b5f3ee9c6b8d50d0658fcd7c4288f9322f9b5
Richard Cook <rcook@rcook.org>
2016-11-04 11:33:40 -0700

    Delete unused files

--- child-health-data/script/exec.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-icu_bin_path=$(dirname $(find /mingw64 -name 'libicu*' | head -1))
-PATH=$icu_bin_path:$PATH child-health-data-app.exe $*
--- child-health-data/script/watch.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-setlocal
-pushd %~dp0..
-stack build --file-watch --exec "sh script/exec.sh %*"
