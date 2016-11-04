c799df6131d19736853008d3ec1828e16dc106c9
Richard Cook <rcook@rcook.org>
2016-11-03 22:18:05 -0700

    Try using a LaTeX template

--- make-doc.cmd
+++ make-doc.cmd
@@ -1,7 +1,8 @@
 @echo off
 setlocal
 set OUTDIR=doc
-set PANDOCOPTS=--highlight-style=tango --latex-engine=xelatex --variable mainfont="Times New Roman"
+set PANDOCOPTSBASE=--highlight-style=tango
+set PANDOCOPTSPDF=--latex-engine=xelatex --variable mainfont="Times New Roman" --template=my.latex
 set INPUTPATHS=index.md part01.md part02.md part03.md part04.md
 set PANDOCDIR=%APPDATA%\local\bin
 set PANDOCPATH=%PANDOCDIR%\pandoc.exe
@@ -10,6 +11,6 @@ del %OUTDIR%\notes.html >nul 2>&1
 del %OUTDIR%\notes.pdf >nul 2>&1
 del %OUTDIR%\notes.tex >nul 2>&1
 
-%PANDOCPATH% %PANDOCOPTS% -s -o %OUTDIR%\notes.html %INPUTPATHS%
-%PANDOCPATH% %PANDOCOPTS% -s -o %OUTDIR%\notes.pdf %INPUTPATHS%
-%PANDOCPATH% %PANDOCOPTS% -s -o %OUTDIR%\notes.tex %INPUTPATHS%
+%PANDOCPATH% %PANDOCOPTSBASE% -s -o %OUTDIR%\notes.html %INPUTPATHS%
+%PANDOCPATH% %PANDOCOPTSBASE% %PANDOCOPTSPDF% -s -o %OUTDIR%\notes.pdf %INPUTPATHS%
+%PANDOCPATH% %PANDOCOPTSBASE% -s -o %OUTDIR%\notes.tex %INPUTPATHS%
