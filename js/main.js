ba67bfca376c23f8b428d1622202bfdddacc29cb
Richard Cook <rcook@rcook.org>
2016-12-31 09:59:34 -0800

    Remove index.html from Home link

--- js/main.js
+++ js/main.js
@@ -2,7 +2,14 @@ function getFileName(url) {
     return url.split("/").pop();
 }
 
+function getLinkHRef(href) {
+    var fileName = getFileName(href);
+    return fileName == ""
+        ? "."
+        : fileName;
+}
+
 $(function() {
-    var fileName = getFileName(window.location.href);
-    $("nav a[href='" + fileName + "']").addClass("disabled").removeAttr("href");
+    var href = getLinkHRef(window.location.href);
+    $("nav a[href='" + href + "']").addClass("disabled").removeAttr("href");
 });
